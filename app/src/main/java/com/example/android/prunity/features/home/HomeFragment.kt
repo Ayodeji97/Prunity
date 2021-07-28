e166a49668532c109536960cbf484a0fc7e4ff44
Ayodeji97 <danielayodeji97@gmail.com>
2021-07-28 09:32:35 +0100

    added tool bar to home framgment

---
+++ app/src/main/java/com/example/android/prunity/features/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.android.prunity.features.home
+
+import androidx.fragment.app.Fragment
+import com.example.android.prunity.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+}
