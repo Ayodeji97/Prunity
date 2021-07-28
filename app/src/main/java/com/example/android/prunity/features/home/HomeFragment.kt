d91d5d613cac17b51e0e0136d3db6a175307731e
Ayodeji97 <danielayodeji97@gmail.com>
2021-07-28 15:54:13 +0100

    Design prunity ui

--- app/src/main/java/com/example/android/prunity/features/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/prunity/features/home/HomeFragment.kt
@@ -1,9 +1,18 @@
 package com.example.android.prunity.features.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.android.prunity.R
+import com.example.android.prunity.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var ui : FragmentHomeBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ui = FragmentHomeBinding.bind(view)
+        ui.fragmentHomeToolbar.appToolbar.setNavigationIcon(R.drawable.arrow)
 
+    }
 }
