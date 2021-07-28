11dee1435a177308a7d89562ac2bb65f6e513602
Ayodeji97 <danielayodeji97@gmail.com>
2021-07-28 11:15:23 +0100

    design home fragment

---
+++ app/src/main/java/com/example/android/prunity/custom/PrunityButton.kt
@@ -0,0 +1,14 @@
+package com.example.android.prunity.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PrunityButton @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+
+
+
+}
